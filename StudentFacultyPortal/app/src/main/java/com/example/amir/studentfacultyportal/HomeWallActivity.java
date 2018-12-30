175aa1d535aabd09cd956fe837a9c599aa12f418
H.M.Zahid <hafizmzahid2@gmail.com>
2018-12-31 03:03:12 +0500

    updated userwall

--- StudentFacultyPortal/app/src/main/java/com/example/amir/studentfacultyportal/HomeWallActivity.java
+++ StudentFacultyPortal/app/src/main/java/com/example/amir/studentfacultyportal/HomeWallActivity.java
@@ -1,4 +1,5 @@
 package com.example.amir.studentfacultyportal;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,81 +11,88 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class HomeWallActivity extends AppCompatActivity {
-    private List<Project> projectList = new ArrayList<>();
+    private List<Project> projectList;
     private RecyclerView recyclerView;
-    private ProjectAdapter mAdapter;
+    private ProjectAdapter projectAdapter;
+    private RequestQueue pQueue;
+    private String basePath;
+    @Override
+    public Context getApplicationContext() {
+        return super.getApplicationContext();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homewall);
+        basePath= getResources().getString(R.string.base_url);
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        mAdapter = new ProjectAdapter(projectList);
-        //recyclerView.setHasFixedSize(true);
-        // vertical RecyclerView
-        // keep movie_list_row.xml width to `match_parent`
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        projectList = new ArrayList<>();
+        pQueue = Volley.newRequestQueue(this);
 
-        // horizontal RecyclerView
-        // keep movie_list_row.xml width to `wrap_content`
-        //RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
-
-        recyclerView.setLayoutManager(mLayoutManager);
-
-        // adding inbuilt divider line
-        //recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
-
-        // adding custom divider line with padding 16dp
-        // recyclerView.addItemDecoration(new MyDividerItemDecoration(this, LinearLayoutManager.HORIZONTAL, 16));
-        //recyclerView.setItemAnimator(new DefaultItemAnimator());
-
-        recyclerView.setAdapter(mAdapter);
-
-        // row click listener
-       /* recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
-            @Override
-            public void onClick(View view, int position) {
-                Movie movie = movieList.get(position);
-                Toast.makeText(getApplicationContext(), movie.getTitle() + " is selected!", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onLongClick(View view, int position) {
-
-            }
-        }));
-        */
-
-        prepareSampleProjectData();
+        ParseProjectJSON();
     }
 
-    private void prepareSampleProjectData() {
-        Project project = new Project("SFP", "Student Faculty Portal", "Devlopment","Muhmmad Amir");
-        projectList.add(project);
-        project = new Project("LMS", "Library Management System", "Devlopment","Hafiz Zahid");
-        projectList.add(project);
-        project = new Project("PMS", "Productivity Measurement System", "Devlopment","Maida Tayyab");
-        projectList.add(project);
-        project = new Project("Driver Assist", "It provides Driver Assist", "Devlopment","Hafiz Irfan");
-        projectList.add(project);
-        project = new Project("EatOrganic", "Buying and Selling of organic", "Devlopment","Muhmmad Amir");
-        projectList.add(project);
-        project = new Project("SFP", "Student Faculty Portal", "Devlopment","Muhmmad Amir");
-        projectList.add(project);
-        project = new Project("LMS", "Library Management System", "Devlopment","Hafiz Zahid");
-        projectList.add(project);
-        project = new Project("PMS", "Productivity Measurement System", "Devlopment","Maida Tayyab");
-        projectList.add(project);
-        project = new Project("Driver Assist", "It provides Driver Assist", "Devlopment","Hafiz Irfan");
-        projectList.add(project);
-        project = new Project("EatOrganic", "Buying and Selling of organic", "Devlopment","Muhmmad Amir");
-        projectList.add(project);
-        mAdapter.notifyDataSetChanged();
+    private void ParseProjectJSON() {
+        String url=basePath + "Project/GetAllProjects";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject data = response.getJSONObject("data");
+                            JSONArray projArray = data.getJSONArray("ProjectList");
+                            Project project;
+                            for (int i = 0; i < projArray.length(); i++) {
+                                JSONObject proj = projArray.getJSONObject(i);
+                                String title = proj.getString("ProjectTitle");
+                                String description = proj.getString("Description");
+                                String intiatedBy = proj.getString("InitiatedBy");
+                                String type = "Development";
+                                project = new Project(title,description,type,intiatedBy);
+                                projectList.add(project);
+                                projectAdapter =new ProjectAdapter(projectList);
+                                recyclerView.setAdapter(projectAdapter);
+                                projectAdapter.notifyDataSetChanged();
+                            }
+                            projectAdapter =new ProjectAdapter(projectList);
+                            recyclerView.setAdapter(projectAdapter);
+                            projectAdapter.notifyDataSetChanged();
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                    error.printStackTrace();
+            }
+        });
+            pQueue.add(request);
     }
+
+
     public void ShareIdea(android.view.View view)
     {
         Toast.makeText(HomeWallActivity.this, "SharIdea Clicked", Toast.LENGTH_LONG).show();
